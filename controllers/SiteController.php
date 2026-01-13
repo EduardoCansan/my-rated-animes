5e5fdc09b50513ca2ed7313a7a8fba21debae18c
EduardoCansan <eduardo.cansann@gmail.com>
2026-01-13 16:52:47 -0300

    feat: add controller logic

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,10 +2,31 @@
 
 namespace app\controllers;
 
-class SiteController
+use app\core\Controller;
+
+class SiteController extends Controllerp
 {
+    public function home()
+    {
+        $params = [
+            'name' => "Dudu"
+        ];
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
     public function handleContact()
     {
+        $body = Application::$app->request->getBody();
+        
+        echo '<pre>';
+        var_dump($body);
+        echo '</pre>';
+
         return 'Handling submitted data';
     }
 }
--- views/contact.php
+++ views/contact.php
@@ -1,16 +1,17 @@
 <h1>Contact</h1>
+
 <form action="" method="post">
   <div class="form-group">
     <label>Subject</label>
     <input type="text" class="form-control" name="subject">
   </div>
   <div class="form-group">
-    <label>Subject</label>
+    <label>Email</label>
     <input type="email" class="form-control" name="email">
   </div>
   <div class="form-group">
     <label>Body</label>
-    <input type="textarea" class="form-control" name="body">
+    <textarea class="form-control" name="body"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
